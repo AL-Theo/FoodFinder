e3b02304dda8fab899f8f1fead27b25296ebc084
Thiago Almeida <43508221+AL-Theo@users.noreply.github.com>
2019-11-13 20:11:19 -0500

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -6,7 +6,7 @@ export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
         Authorization: 
-        'Bearer 0TJOC5fL-rbAeA35nKXcplmFxy40Z4Xt2e7kHi36RGXd6NoV2PRT0gcemFiS4c_BWeXoR-Yq4FOmRWa9SNbOB-KfjgpiKl_gXM6ikQ0bwCSkAztdCe3m8Wil7EimXXYx'
+            'Bearer MYYELPAPIKEY'
     }
 });
 
